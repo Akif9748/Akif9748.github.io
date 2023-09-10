ddaf785067c58f9a0399c8fb7b6025ac0c4fbbda
Akif9748 <akif9748@gmail.com>
2023-09-10 13:58:07 +0300

    Site updated

--- blocker.js
+++
@@ -1,7 +0,0 @@
-document.onkeydown = function (e) {
-    //CHAR LIST:
-    const charList = ['c', 'j', 'ı']
-    if (e.key === "F12" ||
-        e.ctrlKey && (e.key == "u" || e.shiftKey && charList.includes(e.key))
-    ) return false;
-}
